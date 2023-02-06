e1ee2f4540fbdf2702df4983921b76b4c5cfe423
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-02-06 22:27:36 +0100

    Make grid functional

--- scripts.js
+++ scripts.js
@@ -1,12 +1,10 @@
 const grid = document.querySelector("#grid-container");
-const maxGridSquares = prompt('How much squares do you want? (1x1 - 60x60 only)')
+const amountOfGridSquares = prompt(`How much squares do you want (0x0 ratio)`);
 
-for (let i = maxGridSquares * maxGridSquares; i != 0; i--) {
+for (let i = amountOfGridSquares * amountOfGridSquares; i > 0; i--) {
   const gridSquare = document.createElement("div");
   gridSquare.setAttribute("class", "grid-square");
+  gridSquare.style.width = `${960 / amountOfGridSquares}px`;
+  gridSquare.style.height = `${960 / amountOfGridSquares}px`;
   grid.appendChild(gridSquare);
-
-  grid.addEventListener('hover', function(event) {
-    gridSquare.style.backgroundColor = 'green';
-  });
 }
